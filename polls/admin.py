a55d485d688def8e46a58b3d36ec00bd6a97d1a9
t_tamir <t_tamir@126.com>
2017-11-03 08:45:11 +0800

    create the project of polls

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+#admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+admin.site.register(Question, QuestionAdmin)
+
+#Register your models here
