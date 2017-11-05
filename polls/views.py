ef415bcc3cb8d4bb944e4f7c397c8a93924638c6
t_tamir <t_tamir@126.com>
2017-11-05 23:31:51 +0800

    add the three views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world. You're at the polls index!")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s.( the detail method )" % question_id)
+
+def results(request,question_id):
+    response = "You are looking at question %s.( the results method )"
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are looking at question %s.( the vote method )" % question_id)
+
+    
 # Create your views here.
